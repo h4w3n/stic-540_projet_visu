1aeae69aa4c6aa7d47cb7e61610390a7d5e00bdd
h4w3n <hawentran@gmail.com>
2024-12-03 16:33:26 +0100

    début du fichier

---
+++ graphe.R
@@ -0,0 +1,14 @@
+### initialisation ###
+rm(list=ls())
+library(ggplot2)
+
+load("C:/Users/tvh/Documents/ULB/4- master stic/STIC B540 - Visualisation des données et de l'information/projet/data01.RData")
+View(data)
+
+#### Q1 - offre cinématographique au fil du temps ####
+
+
+#### Q2 - facteurs film apprécié par les internautes ####
+
+
+#### Q3 - facteurs film est profitable / rentable ####
